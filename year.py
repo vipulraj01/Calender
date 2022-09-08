283d5d40aa338a94e1653c8972a34877b4e5dbdf
Vipul <vipulraj011@gmail.com>
2022-09-08 13:38:44 +0530

    first commit

---
+++ year.py
@@ -0,0 +1,10 @@
+# Creates a new calendar from a calendar import.
+from calendar import*
+# Enter the year.
+year = int(input("Enter the year : "))
+# Print a calendar.
+print(calendar (year,2,1,8,4))
+# 2 is for the printing of the week name such as (Mo,Tu,We,Th,Fr,Sa,Su)
+# 1 is for the printing of the one line (row) for each week
+# 8 is for the printing of the 8 row for each monnth
+# 4 is for the printing of the 4 coloumn for all monnth of the year
